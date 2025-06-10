3351899d009c504fe9ccb5b8488aea29fa759b6b
TharushikaNethmini <tharunethmini0908@gmail.com>
2025-06-10 12:33:09 +0530

    validation added

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,13 @@
 import express from "express";
 import { signin, signup } from "../controllers/auth.js";
-// import {
-//   isRequestValidated,
-//   validateSigninRequest,
-//   validateSignupRequest,
-// } from "../validators/auth.js";
+import {
+  isRequestValidated,
+  validateSigninRequest,
+  validateSignupRequest,
+} from "../validators/auth.js";
 const router = express.Router();
 
-router.post("/signup", signup);
-// router.post("/signup", validateSignupRequest, isRequestValidated, signup);
-// router.post("/signin", validateSigninRequest, isRequestValidated, signin);
-router.post("/signin", signin);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
 
 export default router;
